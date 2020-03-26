83b12c55e6df14381e4e4e1aeef69b82d05f9596
Spencer Sellers <spencesellers@gmail.com>
2020-03-25 19:40:52 -0500

    Add basic whitespace handling

--- JsonAnalytics/ValueParser.cs
+++ JsonAnalytics/ValueParser.cs
@@ -6,15 +6,17 @@ namespace JsonAnalytics
     {
         public static JsonParser ParserForValue(char c)
         {
+            if (c == ' ') return new ValueParser();
             if (c == '[') return new ArrayParser(ArrayParser.ArrayState.ReadyForFirst);
             if ("-0123456789".Contains(c)) return NumberParser.GetNumberParser(c);
             throw new ArgumentOutOfRangeException("Ruh roh");
         }
 
-        public const string ValueStarts = "0123456789-[{";
+        public const string ValueStarts = "0123456789-[{ ";
 
         public ValueParser()
         {
+            NextChar(' ', ParserForValue);
             NextChar("0123456789-", NumberParser.GetNumberParser);
             NextChar('[', _ => new ArrayParser(ArrayParser.ArrayState.ReadyForFirst));
         }
