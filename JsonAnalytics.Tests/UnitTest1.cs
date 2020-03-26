1d1f1ff01f0a9293859c354c3774491f0ffe169e
Spencer Sellers <spencesellers@gmail.com>
2020-03-25 19:10:48 -0500

    Mostly finish number parsing

--- JsonAnalytics.Tests/UnitTest1.cs
+++ JsonAnalytics.Tests/UnitTest1.cs
@@ -2,19 +2,33 @@ using NUnit.Framework;
 
 namespace JsonAnalytics.Tests
 {
-    public class Tests
+    public class JsonParserTests
     {
+        // Valid
         [TestCase("100", ExpectedResult = true)]
         [TestCase("1000000", ExpectedResult = true)]
         [TestCase("[1,2,3,4]", ExpectedResult = true)]
         [TestCase("[]", ExpectedResult = true)]
         [TestCase("[[1,2,3,4]]", ExpectedResult = true)]
+        [TestCase("-150", ExpectedResult = true)]
+        [TestCase("0", ExpectedResult = true)]
+        [TestCase("1.6e10", ExpectedResult = true)]
+        [TestCase("1.6", ExpectedResult = true)]
+        [TestCase("5e5", ExpectedResult = true)]
+        [TestCase("[1.1,2e2,3.3e3,4e40]", ExpectedResult = true)]
         
+        // Invalid
         [TestCase("[1,2,3,4", ExpectedResult = false)]
         [TestCase("[", ExpectedResult = false)]
         [TestCase("1,2,3,4", ExpectedResult = false)]
         [TestCase("]", ExpectedResult = false)]
         [TestCase("[1[2]", ExpectedResult = false)]
+        [TestCase("01", ExpectedResult = false)]
+        [TestCase("1.6e10e2", ExpectedResult = false)]
+        [TestCase("1e2e3", ExpectedResult = false)]
+        [TestCase("1e2.2", ExpectedResult = false)]
+        [TestCase("1e", ExpectedResult = false)]
+        [TestCase("1.", ExpectedResult = false)]
         public bool CanTellIfJsonIsValid(string input)
         {
             return new JsonHandler().IsValidJson(input);
--- JsonAnalytics/Program.cs
+++ JsonAnalytics/Program.cs
@@ -65,12 +65,82 @@ namespace JsonAnalytics
 
     public class NumberParser : JsonParser
     {
-        public NumberParser()
+        private readonly NumberState _state;
+
+        public static NumberParser GetNumberParser(char c)
         {
-            NextChar("0123456789", _ => new NumberParser());
+            if (c == '0')
+            {
+                return new NumberParser(NumberState.IsZero);
+            }
+            
+            if (c == '-')
+            {
+                return new NumberParser(NumberState.ReadyForFirstDigit);
+            }
+
+            if ("123456789".Contains(c))
+            {
+                return new NumberParser(NumberState.SecondaryDigit);
+            }
+            
+            throw new ArgumentException("Bad Number start: " + c);
+        }
+        
+        public enum NumberState
+        {
+            IsZero,
+            ReadyForFirstDigit,
+            SecondaryDigit,
+            ReadyForFraction,
+            ReadyToContinueFraction,
+            ReadyForExponentStart,
+            ReadyToContinueExponent
+        }
+        public NumberParser(NumberState state)
+        {
+            _state = state;
+            switch (state)
+            {
+                case NumberState.IsZero:
+                    break;
+                case NumberState.ReadyForFirstDigit:
+                    NextChar("123456789", _ => new NumberParser(NumberState.SecondaryDigit));
+                    break;
+                case NumberState.SecondaryDigit:
+                    NextChar("0123456789", _ => new NumberParser(NumberState.SecondaryDigit));
+                    NextChar(".", _ => new NumberParser(NumberState.ReadyForFraction));
+                    NextChar("eE", _ => new NumberParser(NumberState.ReadyForExponentStart));
+                    break;
+                case NumberState.ReadyForFraction:
+                    NextChar("0123456789", _ => new NumberParser(NumberState.ReadyToContinueFraction));
+                    break;
+                case NumberState.ReadyToContinueFraction:
+                    NextChar("0123456789", _ => new NumberParser(NumberState.ReadyToContinueFraction));
+                    NextChar("eE", _ => new NumberParser(NumberState.ReadyForExponentStart));
+                    break;
+                case NumberState.ReadyForExponentStart:
+                    NextChar("0123456789", _ => new NumberParser(NumberState.ReadyToContinueExponent));
+                    break;
+                case NumberState.ReadyToContinueExponent:
+                    NextChar("0123456789", _ => new NumberParser(NumberState.ReadyToContinueExponent));
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
         }
 
-        public override bool CanComplete => true;
+        public override bool CanComplete => _state switch
+        {
+            NumberState.IsZero => true,
+            NumberState.ReadyForFirstDigit => false,
+            NumberState.SecondaryDigit => true,
+            NumberState.ReadyForFraction => false,
+            NumberState.ReadyToContinueFraction => true,
+            NumberState.ReadyForExponentStart => false,
+            NumberState.ReadyToContinueExponent => true,
+        };
     }
 
     public class ArrayParser : JsonParser
@@ -116,15 +186,15 @@ namespace JsonAnalytics
         public static JsonParser ParserForValue(char c)
         {
             if (c == '[') return new ArrayParser(ArrayParser.ArrayState.ReadyForFirst);
-            if ("0123456789".Contains(c)) return new NumberParser();
+            if ("-0123456789".Contains(c)) return NumberParser.GetNumberParser(c);
             throw new ArgumentOutOfRangeException("Ruh roh");
         }
 
-        public static readonly string ValueStarts = "0123456789[{";
+        public const string ValueStarts = "0123456789-[{";
 
         public ValueParser()
         {
-            NextChar("0123456789", _ => new NumberParser());
+            NextChar("0123456789-", NumberParser.GetNumberParser);
             NextChar('[', _ => new ArrayParser(ArrayParser.ArrayState.ReadyForFirst));
         }
 
