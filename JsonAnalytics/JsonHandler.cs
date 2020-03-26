6136137667d559f9b92560e4349887e5b193f4a6
Spencer Sellers <spencesellers@gmail.com>
2020-03-25 21:02:16 -0500

    Add bfs to look for certain values

--- JsonAnalytics/JsonHandler.cs
+++ JsonAnalytics/JsonHandler.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace JsonAnalytics
@@ -18,6 +20,57 @@ namespace JsonAnalytics
             }
 
             return parser.CanBeTheEndOfInput;
+        }
+
+        private class BfsNode
+        {
+            public string Json;
+            public JsonParser Parser;
+        }
+
+        private IEnumerable<BfsNode> NextStates(BfsNode node)
+        {
+            return node.Parser.AcceptableChars().Select(c => new BfsNode
+            {
+                Json = node.Json + c,
+                Parser = node.Parser.Read(c)
+            });
+        }
+
+        private static void QueueAll<T>(Queue<T> queue, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                queue.Enqueue(item);
+            }
+        }
+
+        public void Bfs()
+        {
+            var initialParser = new ValueParser();
+            var initialStates = initialParser.AcceptableChars().Select(c => new BfsNode
+            {
+                Json = c.ToString(),
+                Parser = initialParser.Read(c)
+            });
+            var queue = new Queue<BfsNode>();
+            QueueAll(queue, initialStates);
+
+            while (queue.Any())
+            {
+                var next = queue.Dequeue();
+                if (next.Parser.CanBeTheEndOfInput)
+                {
+                    Console.Out.WriteLine(next.Json);                    
+                }
+                if (next.Json.Length > 40)
+                {
+                    return;
+                }
+                
+                var nextStates = NextStates(next);
+                QueueAll(queue, nextStates); // TODO: Filter nextStates for seen before (via hash?)
+            }
         }
     }
 }
--- JsonAnalytics/Program.cs
+++ JsonAnalytics/Program.cs
@@ -16,27 +16,28 @@ namespace JsonAnalytics
             // var nexts = string.Join("", p4.AcceptableChars());
             // Console.Out.WriteLine(nexts);
 
-            for (var n = 0; n < 10; n++)
-            {
-                var s = "";
-                JsonParser parser = new ValueParser();
-                for (var i = 0; i < 100; i++)
-                {
-                    // var nextChar = RandomElement(parser.AcceptableChars());
-                    var nextChars = parser.AcceptableChars().ToList();
-                    if (!nextChars.Any())
-                    {
-                        break;
-                    }
-
-                    var next = RandomElement(nextChars);
-                    s += next;
-                    parser = parser.Read(next);
-                }
-                
-                Console.Out.WriteLine(s);
-            }
-
+            // for (var n = 0; n < 20; n++)
+            // {
+            //     var s = "[";
+            //     JsonParser parser = new ValueParser().Read('[');
+            //     for (var i = 0; i < 2500; i++)
+            //     {
+            //         // var nextChar = RandomElement(parser.AcceptableChars());
+            //         var nextChars = parser.AcceptableChars().ToList();
+            //         if (!nextChars.Any())
+            //         {
+            //             break;
+            //         }
+            //
+            //         var next = RandomElement(nextChars);
+            //         s += next;
+            //         parser = parser.Read(next);
+            //     }
+            //     
+            //     Console.Out.WriteLine(s);
+            // }
+            
+            new JsonHandler().Bfs();
         }
 
         private static T RandomElement<T>(IList<T> items)
